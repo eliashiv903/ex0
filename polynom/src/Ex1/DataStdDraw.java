6c495a1620ac52bc2f87e990f242b6fc20bac64e
yoaveliashiv <57868000+yoaveliashiv@users.noreply.github.com>
2019-12-30 16:00:56 +0200

    Add files via upload

---
+++ polynom/src/Ex1/DataStdDraw.java
@@ -0,0 +1,26 @@
+package Ex1;
+
+public class DataStdDraw {
+
+	private int Width;
+	private int Height;
+	private int Resolution;
+	private double [] Range_X= new double[2];
+	private double [] Range_Y=new double[2];
+	
+public int getWidth () {
+	return this.Width;
+}
+public int getHeight () {
+	return this.Height;
+}
+public int getResolution () {
+	return this.Resolution;
+}
+public double[] getRange_X() {
+	return this.Range_X;
+}
+public double[] getRange_Y() {
+	return this.Range_Y;
+}
+}
